b7772278f493067f17ab30d4925aa9391243073e
marrasluca <lmarras10@gmail.com>
2025-02-13 19:11:43 +0100

    add: router

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const postRouter = require('./routers/postsRouter'); //Importa un router per gestire le operazioni relative ai post
 
+app.use(express.static('public')); //cartella statica
+
+app.get('/', (req, res) => {
+    res.send('Ciao a tutti!');
+})
+
+// app.use('api/posts', postRouter); //router
+
+
+//avvio del server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
